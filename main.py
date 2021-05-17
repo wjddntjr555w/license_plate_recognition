6204ea67a66df1d4e4d18ff2434608f192f4e81f
Jeong WooSeok <wjddntjr555w@naver.com>
2021-05-17 13:06:35 +0900

    change find

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ MAX_HEIGHT_DIFF = 0.2
 MIN_N_MATCHED = 3
 
 # image Info
-img = cv2.imread('33.jpg')
+img = cv2.imread('333.jpg')
 height, width, channel = img.shape
 
 #Final image
@@ -36,17 +36,17 @@ def initial():
 
 
 def gaussian_blur(gray):
-    structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    # 엣지 검출을 위한 커널 행렬 설정 (모양, 크기)
-    imgTopHat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, structuringElement)
-    imgBlackHat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, structuringElement)
-    #모폴로지 연산, (원본배열, 연산방법, 커널) 경계선을 검출하기 위한 사전 작업
-    # tophat = 원본 - 열림(침식 + 팽창) : 밝은 영역 강조
-    # blackhat = 닫힘(팽창 + 침식) - 원본 : 어두운 부분 강조
-
-    imgGrayscalePlusTopHat = cv2.add(gray, imgTopHat)
-    gray = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
-    #tophat, blackhat 적용
+    # structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    # # 엣지 검출을 위한 커널 행렬 설정 (모양, 크기)
+    # imgTopHat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, structuringElement)
+    # imgBlackHat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, structuringElement)
+    # #모폴로지 연산, (원본배열, 연산방법, 커널) 경계선을 검출하기 위한 사전 작업
+    # # tophat = 원본 - 열림(침식 + 팽창) : 밝은 영역 강조
+    # # blackhat = 닫힘(팽창 + 침식) - 원본 : 어두운 부분 강조
+    #
+    # imgGrayscalePlusTopHat = cv2.add(gray, imgTopHat)
+    # gray = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+    # #tophat, blackhat 적용
 
     img_blurred = cv2.GaussianBlur(gray, ksize=(5, 5), sigmaX=0)
     #가우시안 블러 적용, 윤곽선을 더 잘 잡을 수 있도록 한다.
@@ -96,7 +96,7 @@ def threshold(img_blurred):
     return contours, img_thresh, temp_result
 
 def canny(img_blurred):
-    img_canny = cv2.Canny(img_blurred, 100, 200)
+    img_canny = cv2.Canny(img_blurred, 200, 300)
 
     contours, _ = cv2.findContours(img_canny,
                                    mode=cv2.RETR_LIST,
@@ -160,7 +160,7 @@ MIN_GRADIENT = 0.25
 MIN_PADDING_X = 10
 MAX_PADDING_X = 150
 PADDING_Y = 30
-def findLicensePlate_2(contours_dict,img_blur):
+def findLicensePlate(contours_dict,img_blur):
     find_contours = []
 
     cnt = 0
@@ -220,7 +220,7 @@ def findLicensePlate_2(contours_dict,img_blur):
 
     resize_plate = cv2.resize(number_plate, None, fx=1.8, fy=1.8, interpolation=cv2.INTER_CUBIC + cv2.INTER_LINEAR)
     # plate_gray = cv2.cvtColor(resize_plate, cv2.COLOR_BGR2GRAY)
-    ret, th_plate = cv2.threshold(resize_plate, 150, 255, cv2.THRESH_BINARY)
+    _, th_plate = cv2.threshold(resize_plate, 150, 255, cv2.THRESH_BINARY)
 
     plt.figure(1)
     plt.subplot(2, 2, 1)
@@ -237,225 +237,15 @@ def findLicensePlate_2(contours_dict,img_blur):
             result_chars += c
     print(result_chars)
 
-def findLicensePlate(contours_dict):
-    possible_contours = []
-
-    cnt = 0
-    for d in contours_dict:
-        area = d['w'] * d['h']
-        ratio = d['w'] / d['h']
-
-        if area > MIN_AREA \
-                and d['w'] > MIN_WIDTH and d['h'] > MIN_HEIGHT \
-                and MIN_RATIO < ratio < MAX_RATIO:
-            d['idx'] = cnt
-            cnt += 1
-            possible_contours.append(d)
-
-    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
-
-    for d in possible_contours:
-        cv2.rectangle(temp_result, pt1=(d['x'], d['y']), pt2=(d['x'] + d['w'], d['y'] + d['h']),
-                      color=(255, 255, 255), thickness=2)
-    #번호판으로 추정되는 배열들 선택
-
-
-    def find_chars(contour_list):
-        matched_result_idx = []
-
-        for d1 in contour_list:
-            matched_contours_idx = []
-            for d2 in contour_list:
-                if d1['idx'] == d2['idx']:
-                    continue
-
-                dx = abs(d1['cx'] - d2['cx'])
-                dy = abs(d1['cy'] - d2['cy'])
-
-                diagonal_length1 = np.sqrt(d1['w'] ** 2 + d1['h'] ** 2)
-
-                distance = np.linalg.norm(np.array([d1['cx'], d1['cy']]) - np.array([d2['cx'], d2['cy']]))
-                if dx == 0:
-                    angle_diff = 90
-                else:
-                    angle_diff = np.degrees(np.arctan(dy / dx))
-                area_diff = abs(d1['w'] * d1['h'] - d2['w'] * d2['h']) / (d1['w'] * d1['h'])
-                width_diff = abs(d1['w'] - d2['w']) / d1['w']
-                height_diff = abs(d1['h'] - d2['h']) / d1['h']
-
-                if distance < diagonal_length1 * MAX_DIAG_MULTIPLYER \
-                        and angle_diff < MAX_ANGLE_DIFF and area_diff < MAX_AREA_DIFF \
-                        and width_diff < MAX_WIDTH_DIFF and height_diff < MAX_HEIGHT_DIFF:
-                    matched_contours_idx.append(d2['idx'])
-
-            matched_contours_idx.append(d1['idx'])
-
-            if len(matched_contours_idx) < MIN_N_MATCHED:
-                continue
-
-            matched_result_idx.append(matched_contours_idx)
-
-            unmatched_contour_idx = []
-            for d4 in contour_list:
-                if d4['idx'] not in matched_contours_idx:
-                    unmatched_contour_idx.append(d4['idx'])
-
-            unmatched_contour = np.take(possible_contours,
-                                        unmatched_contour_idx)
-
-            recursive_contour_list = find_chars(unmatched_contour)
-
-            for idx in recursive_contour_list:
-                matched_result_idx.append(idx)
-
-            break
-
-        return matched_result_idx
-
-
-    result_idx = find_chars(possible_contours)
-
-    matched_result = []
-    for idx_list in result_idx:
-        matched_result.append(np.take(possible_contours, idx_list))
-
-    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
-
-    for r in matched_result:
-        for d in r:
-            cv2.rectangle(temp_result, pt1=(d['x'], d['y']),
-                          pt2=(d['x'] + d['w'], d['y'] + d['h']),
-                          color=(255, 255, 255), thickness=2)
-    #가장 번호판으로 추측되는 배열 선택
-
-    return matched_result, temp_result
-
-
-def imgCrop(matched_result):
-    for i, matched_chars in enumerate(matched_result):
-        sorted_chars = sorted(matched_chars, key=lambda x: x['cx'])
-
-        plate_cx = (sorted_chars[0]['cx'] + sorted_chars[-1]['cx']) / 2
-        plate_cy = (sorted_chars[0]['cy'] + sorted_chars[-1]['cy']) / 2
-
-        plate_width = (sorted_chars[-1]['x'] + sorted_chars[-1]['w'] - sorted_chars[0]['x']) * PLATE_WIDTH_PADDING
-
-        sum_height = 0
-        for d in sorted_chars:
-            sum_height += d['h']
-
-        plate_height = int(sum_height / len(sorted_chars) * PLATE_HEIGHT_PADDING)
-
-        triangle_height = sorted_chars[-1]['cy'] - sorted_chars[0]['cy']
-        triangle_hypotenus = np.linalg.norm(
-            np.array([sorted_chars[0]['cx'], sorted_chars[0]['cy']]) -
-            np.array([sorted_chars[-1]['cx'], sorted_chars[-1]['cy']])
-        )
-
-        angle = np.degrees(np.arcsin(triangle_height / triangle_hypotenus))
-
-        rotation_matrix = cv2.getRotationMatrix2D(center=(plate_cx, plate_cy), angle=angle, scale=1.0)
-
-        img_rotated = cv2.warpAffine(img_thresh, M=rotation_matrix, dsize=(width, height))
-
-        img_cropped = cv2.getRectSubPix(
-            img_rotated,
-            patchSize=(int(plate_width), int(plate_height)),
-            center=(int(plate_cx), int(plate_cy))
-        )
-
-        if img_cropped.shape[1] / img_cropped.shape[0] < MIN_PLATE_RATIO or img_cropped.shape[1] / img_cropped.shape[0] < MIN_PLATE_RATIO > MAX_PLATE_RATIO:
-            continue
-
-        plate_imgs.append(img_cropped)
-        plate_infos.append({
-            'x': int(plate_cx - plate_width / 2),
-            'y': int(plate_cy - plate_height / 2),
-            'w': int(plate_width),
-            'h': int(plate_height)
-        })
-    # 번호판이미지만 보정 후 잘라내기
-
-    return img_cropped, plate_imgs
-
-
-def imgToChar(plate_imgs):
-    for i, plate_img in enumerate(plate_imgs):
-        plate_img = cv2.resize(plate_img, dsize=(0, 0), fx=1.6, fy=1.6)
-        _, plate_img = cv2.threshold(plate_img, thresh=0.0, maxval=255.0, type=cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-        contours, _ = cv2.findContours(plate_img, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
-        plate_min_x, plate_min_y = plate_img.shape[1], plate_img.shape[0]
-        plate_max_x, plate_max_y = 0, 0
-
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-
-            area = w * h
-            ratio = w / h
-
-            if area > MIN_AREA \
-                    and w > MIN_WIDTH and h > MIN_HEIGHT \
-                    and MIN_RATIO < ratio < MAX_RATIO:
-                if x < plate_min_x:
-                    plate_min_x = x
-                if y < plate_min_y:
-                    plate_min_y = y
-                if x + w > plate_max_x:
-                    plate_max_x = x + w
-                if y + h > plate_max_y:
-                    plate_max_y = y + h
-
-        img_result = plate_img[plate_min_y:plate_max_y, plate_min_x:plate_max_x]
-
-        img_result = cv2.GaussianBlur(img_result, ksize=(3, 3), sigmaX=0)
-        _, img_result = cv2.threshold(img_result, thresh=0.0, maxval=255.0, type=cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        img_result = cv2.copyMakeBorder(img_result, top=10, bottom=10, left=10, right=10, borderType=cv2.BORDER_CONSTANT,
-                                        value=(0, 0, 0))
-
-        chars = pytesseract.image_to_string(img_result, lang='kor', config='--psm 7 --oem 0')
-
-        result_chars = ''
-        has_digit = False
-        for c in chars:
-            if ord('가') <= ord(c) <= ord('힣') or c.isdigit():
-                if c.isdigit():
-                    has_digit = True
-                result_chars += c
-
-        # print(result_chars)
-        plate_chars.append(result_chars)
-
-        if has_digit and len(result_chars) > longest_text:
-            longest_idx = i
-
-    return img_result
-    # 번호판 사진에서 글자들 식별
-
-def printImgChar():
-    info = plate_infos[longest_idx]
-    chars = plate_chars[longest_idx]
-
-    print(chars)
-
-    img_out = img.copy()
-
-    cv2.rectangle(img_out, pt1=(info['x'], info['y']), pt2=(info['x']+info['w'], info['y']+info['h']), color=(255,0,0), thickness=2)
-
-    cv2.imwrite(chars + '.jpg', img_out)
-    # 최종식별 후 처음이미지에서 번호판 부분만 빨간색 네모로 표시
-
-
-    return img_out
-
 
 
 #initial image color to gray.
 img_blurred = initial()
 
+
 #threshold
-contours, img_thresh, temp_result = threshold(img_blurred) # AdaptiveThreshold
-# contours, img_thresh, temp_result = canny(img_blurred) #canny edge
+# contours, img_thresh, temp_result = threshold(img_blurred) # AdaptiveThreshold
+contours, img_thresh, temp_result = canny(img_blurred) #canny edge
 # contours, img_thresh, temp_result = sobel(img_blurred) #sobel edge
 
 # plt.figure(1)
@@ -472,26 +262,7 @@ plt.figure(1)
 plt.subplot(2,2,3)
 plt.imshow(temp_result)
 
-#findLicensePlate
-matched_result, temp_result = findLicensePlate(contours_dict)
+findLicensePlate(contours_dict,img_blurred)
 
-#imgCrop
-img_cropped, plate_imgs = imgCrop(matched_result)
-
-#imgToChar
-img_result = imgToChar(plate_imgs)
-
-plt.figure(1)
-plt.subplot(2,2,4)
-plt.imshow(img_result)
-
-#printImgChar
-img_out = printImgChar()
-
-findLicensePlate_2(contours_dict,img_blurred)
-
-# plt.figure(1)
-# plt.subplot(2,2,4)
-# plt.imshow(img_out)
 
 plt.show()
